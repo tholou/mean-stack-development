287dff61addbcf94286884e21599282ccebe15df
Eyinade Tolu <tholou4reel@gmail.com>
2017-03-24 13:39:25 +0200

    Authentication with JWT

--- sample-node-api/server.js
+++ sample-node-api/server.js
@@ -6,6 +6,7 @@
 // ======================================
 
 // CALL THE PACKAGES --------------------
+require('dotenv').config();
 var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser'); // get body-parser
@@ -13,6 +14,8 @@ var morgan = require('morgan'); // used to see requests
 var mongoose = require('mongoose'); // for working w/ our database
 var port = process.env.PORT || 8080; // set the port for our app
 var User = require('../sample-node-api/models/user');
+var jwt = require('jsonwebtoken');
+var superSecret = process.env.JWT_SECRET;
 
 // APP CONFIGURATION ---------------------
 // use body parser so we can grab information from POST requests
@@ -45,18 +48,112 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// route for authenticating users
+apiRouter.post('/authenticate', function (req, res) {
+
+    // find the user
+    // select the name username and password explicitly
+    User.findOne({
+        username: req.body.username
+    }).select('name username password').exec(function (err, user) {
+        if (err) throw err;
+        //
+
+        // no user with that username was found
+        if (!user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed, User not found.'
+            });
+        } else if (user) {
+
+            // check if password matches
+            var validPassword = user.comparePassword(req.body.password);
+
+            if (!validPassword){
+                res.json({
+                    success: false,
+                    message: 'Authentication failed, Wrong Password'
+                });
+            } else {
+
+                // if user found and password is right
+                // create token
+                var token = jwt.sign({
+                    name: user.name,
+                    username: user.username
+                }, superSecret, {
+                    expiresIn : 60*60*24 //expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+
+    });
+});
+
 //middleware to use for all requests
-apiRouter.use(function (req, res, next) {
+/*apiRouter.use(function (req, res, next) {
+ // do logging
+ console.log('Somebody just came to my app!');
+ next(); //make sure we go to the next route/middleware and don't stop here
+
+ });*/
+
+// route middleware to verify a token
+apiRouter.use(function(req, res, next) {
+
     // do logging
     console.log('Somebody just came to my app!');
-    next(); //make sure we go to the next route/middleware and don't stop here
 
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, superSecret, function(err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+
+                next();
+            }
+        });
+    } else {
+
+        // if there is no token
+        // return an HTTP response of 403 (access forbidden) and an error message
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+    // next() used to be here
 });
 
 // test route to make sure everything is working
 // accessed at GET http://localhost:8080/api
 apiRouter.get('/', function(req, res) {
     res.json({ message: 'Hooray! Welcome to my First Node API!' });
+});
+
+// api endpoint to get user information
+apiRouter.get('/me', function(req, res) {
+    res.send(req.decoded);
 });
 
 // on routes that end in /users
